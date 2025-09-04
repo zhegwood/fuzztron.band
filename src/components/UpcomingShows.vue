afa0d898f1072d2df44574889797ea544a388fd8
Zach <zhegwood@gmail.com>
2025-09-04 15:21:10 -0600

    added a show

--- src/components/UpcomingShows.vue
+++ src/components/UpcomingShows.vue
@@ -6,5 +6,6 @@
     <h2 class="mb-2 text-2xl">Upcoming Shows</h2>
     <p class="mb-1">9/6/2025 - Tres Litros - 7-10pm - Salida, CO</p>
     <p class="mb-1">10/18/2025 - The Vic - 9-12pm - Salida, CO</p>
+    <p class="mb-1">11/1/2025 - Elevation Brewing - 5-8pm - Poncha Springs, CO</p>
   </div>
 </template>
