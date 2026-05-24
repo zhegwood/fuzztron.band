ae659bffa1b0b68f588022d83866e32bd406b00b
Zach <zhegwood@gmail.com>
2026-05-24 08:14:18 -0600

    new show

--- src/components/UpcomingShows.vue
+++ src/components/UpcomingShows.vue
@@ -6,6 +6,7 @@
     <h2 class="mb-2 text-2xl">Upcoming Shows</h2>
     <ul class="flex flex-col gap-1">
       <li>5/30/2026 - Tres Litros Brewing - 7-10pm - Salida, CO</li>
+      <li>6/13/2026 - Elevation Brewing - 2-4pm - Poncha Springs, CO</li>
       <li>6/13/2026 - Wood's Distillery - 8-11pm - Salida, CO</li>
       <li>7/3/2026 - Velveteen - 7-10pm - Salida, CO</li>
       <li>7/24/2026 - Fun Street Family Arcade - 8-10pm - Salida, CO</li>
