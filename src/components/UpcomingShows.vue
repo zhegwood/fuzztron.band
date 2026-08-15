e20d863bae3c240223d90cd9c19a183c6c55f636
Zach <zhegwood@gmail.com>
2026-08-15 10:00:41 -0600

    updated shows

--- src/components/UpcomingShows.vue
+++ src/components/UpcomingShows.vue
@@ -5,8 +5,7 @@
   <div>
     <h2 class="mb-2 text-2xl">Upcoming Shows</h2>
     <ul class="flex flex-col gap-1">
-      <li>7/24/2026 - Wood's Distillery - 7-10pm - Salida, CO</li>
-      <li>8/13/2026 - Main Street Tavern - 7-10pm - Buena Vista, CO</li>
+      <li>Nothing?! <a mailto="info@fuzztron.band">Hit us up!</a></li>
     </ul>
   </div>
 </template>
--- src/views/BookingView.vue
+++ src/views/BookingView.vue
@@ -12,35 +12,39 @@
     <UpcomingShows class="mb-4" />
     <div class="mb-4">
       <h2 class="mb-2 text-2xl">Past Shows</h2>
-      <p class="mb-1">7/3/2026 - The Velveteen - Salida, CO</p>
-      <p class="mb-1">6/13/2026 - Wood's Distillery - Salida, CO</p>
-      <p class="mb-1">6/13/2026 - Elevation Brewing Co - Salida, CO</p>
-      <p class="mb-1">5/30/2026 - Tres Litros Brewing - Salida, CO</p>
-      <p class="mb-1">3/27/2026 - The Vic - Salida, CO</p>
-      <p class="mb-1">3/7/2026 - Wood's Distillery - Salida, CO</p>
-      <p class="mb-1">11/1/2025 - Elevation Brewing - Poncha Springs, CO</p>
-      <p class="mb-1">10/17/2025 - The Vic - Salida, CO</p>
-      <p class="mb-1">9/6/2025 - Tres Litros - Salida, CO</p>
-      <p class="mb-1">7/5/2025 - The Velveteen - Salida, CO</p>
-      <p class="mb-1">6/14/2025 - FIBArk! The Vic - Salida, CO</p>
-      <p class="mb-1">6/12/2025 - FIBArk! Riverside Park - Salida, CO</p>
-      <p class="mb-1">5/17/2025 - Big Damn Jam - A-Church Salida, CO</p>
-      <p class="mb-1">3/22/2025 - Wood's Distillery - Salida, CO</p>
-      <p class="mb-1">2/22/2025 - Private Party - Leadville, CO</p>
-      <p class="mb-1">1/11/2025 - Main Street Tavern - Buena Vista, CO</p>
-      <p class="mb-1">12/13/2024 - Tres Litros Beer Co - Salida, CO</p>
-      <p class="mb-1">12/7/2024 - Private Party - Salida, CO</p>
-      <p class="mb-1">11/8/2024 - Victoria Tavern - Salida, CO</p>
-      <p class="mb-1">10/12/2024 - Wood's Distillery - Salida, CO</p>
-      <p class="mb-1">9/13/2024 - The Velveteen - Salida, CO</p>
-      <p class="mb-1">8/31/2024 - Tres Litros Beer Co - Salida, CO</p>
-      <p class="mb-1">7/19/2024 - Pastime Saloon - Leadville, CO</p>
-      <p class="mb-1">7/13/2024 - The Velveteen - Salida, CO</p>
-      <p class="mb-1">6/15/2024 - FIBArk! - Wood's Distillery - Salida, CO</p>
-      <p class="mb-1">5/11/2024 - The Velveteen - Salida, CO</p>
-      <p class="mb-1">4/27/2024 - Tres Litros Beer Co - Salida, CO</p>
-      <p class="mb-1">4/13/2024 - Wood's Distillery - Salida, CO</p>
-      <p class="mb-1">12/31/2023 - A-Church - Salida, CO</p>
+      <ul class="flex flex-col gap-1 p-0 list-none">
+        <li>7/24/2026 - Wood's Distillery - Salida, CO</li>
+        <li>8/13/2026 - Main Street Tavern - Buena Vista, CO</li>
+        <li>7/3/2026 - The Velveteen - Salida, CO</li>
+        <li>6/13/2026 - Wood's Distillery - Salida, CO</li>
+        <li>6/13/2026 - Elevation Brewing Co - Salida, CO</li>
+        <li>5/30/2026 - Tres Litros Brewing - Salida, CO</li>
+        <li>3/27/2026 - The Vic - Salida, CO</li>
+        <li>3/7/2026 - Wood's Distillery - Salida, CO</li>
+        <li>11/1/2025 - Elevation Brewing - Poncha Springs, CO</li>
+        <li>10/17/2025 - The Vic - Salida, CO</li>
+        <li>9/6/2025 - Tres Litros - Salida, CO</li>
+        <li>7/5/2025 - The Velveteen - Salida, CO</li>
+        <li>6/14/2025 - FIBArk! The Vic - Salida, CO</li>
+        <li>6/12/2025 - FIBArk! Riverside Park - Salida, CO</li>
+        <li>5/17/2025 - Big Damn Jam - A-Church Salida, CO</li>
+        <li>3/22/2025 - Wood's Distillery - Salida, CO</li>
+        <li>2/22/2025 - Private Party - Leadville, CO</li>
+        <li>1/11/2025 - Main Street Tavern - Buena Vista, CO</li>
+        <li>12/13/2024 - Tres Litros Beer Co - Salida, CO</li>
+        <li>12/7/2024 - Private Party - Salida, CO</li>
+        <li>11/8/2024 - Victoria Tavern - Salida, CO</li>
+        <li>10/12/2024 - Wood's Distillery - Salida, CO</li>
+        <li>9/13/2024 - The Velveteen - Salida, CO</li>
+        <li>8/31/2024 - Tres Litros Beer Co - Salida, CO</li>
+        <li>7/19/2024 - Pastime Saloon - Leadville, CO</li>
+        <li>7/13/2024 - The Velveteen - Salida, CO</li>
+        <li>6/15/2024 - FIBArk! - Wood's Distillery - Salida, CO</li>
+        <li>5/11/2024 - The Velveteen - Salida, CO</li>
+        <li>4/27/2024 - Tres Litros Beer Co - Salida, CO</li>
+        <li>4/13/2024 - Wood's Distillery - Salida, CO</li>
+        <li>12/31/2023 - A-Church - Salida, CO</li>
+      </ul>
     </div>
   </div>
 </template>
