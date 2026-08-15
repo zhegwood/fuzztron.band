606275080a4af0f9d5c529f216d8bb966dd52a4a
Zach <zhegwood@gmail.com>
2026-08-15 10:03:08 -0600

    updated shows

--- src/components/UpcomingShows.vue
+++ src/components/UpcomingShows.vue
@@ -5,7 +5,7 @@
   <div>
     <h2 class="mb-2 text-2xl">Upcoming Shows</h2>
     <ul class="flex flex-col gap-1">
-      <li>Nothing?! <a mailto="info@fuzztron.band">Hit us up!</a></li>
+      <li>12/31/2026 - Wood's Distillery - 9-12pm - Salida, CO</li>
     </ul>
   </div>
 </template>
